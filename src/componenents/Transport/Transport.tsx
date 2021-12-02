9d7dd3e609c7e9f3f860903248ab7740a9c133b6
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2021-12-02 17:10:14 +0000

    Transport: implement basic design

--- src/componenents/Transport/Transport.tsx
+++ src/componenents/Transport/Transport.tsx
@@ -1,7 +1,31 @@
+import "./Transport.scss";
+import stop from "../../images/stop.svg";
+import play from "../../images/play.svg";
+
 export default function Transport() {
   return (
     <div className="Transport">
-      Transport
+      <div className="Transport__grid">
+        <div className="Transport__grid__left">
+          <input type="range" min="0" max="100" step="1"></input>
+          <div className="volume-feedback"></div>
+        </div>
+
+        <div className="Transport__grid__center">
+          <button className="button__track-forwards"></button>
+          <button className="button__stop">
+            <img src={stop}></img>
+          </button>
+          <button className="button__play-pause">
+            <img src={play}></img>
+          </button>
+          <button className="button__track-backwards"></button>
+        </div>
+
+        <div className="Transport__grid__right">
+          <input value="120"></input>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
