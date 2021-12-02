78d122b10181847316f178cd8e70dcb9a871bd96
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2021-12-02 14:47:08 +0000

    Initialise app, add empty transport component

---
+++ src/componenents/App/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import '../Transport/Transport'
+import Transport from '../Transport/Transport';
+
+function App() {
+  return (
+    <div className="App">
+      <Transport />
+    </div>
+  );
+}
+
+export default App;
