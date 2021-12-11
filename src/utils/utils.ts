0bd59e97b314e59a3e8171b1a2a187c1bf0f53a4
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2021-12-11 00:44:39 +0000

    Add utility functions to convert amplitude to db
    Also added one to convert db to amplitude

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,11 @@ export function getVariableStyle(varName: string): string {
   const style: CSSStyleDeclaration = getComputedStyle(document.body);
   return style.getPropertyValue(varName);
 }
+
+export function amplitudeToDecibels(amplitude: number): number {
+  return (20 * Math.log(amplitude)) / Math.LN10;
+}
+
+export function decibelsToAmplitude(decibels: number): number {
+  return Math.pow(10, decibels / 20);
+}
