6d39303837a489548cc45897b2a939afaa156087
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2021-12-17 14:28:41 +0000

    Fix lower and upper value limits in range slider
    Stops value going below 0 or 144 - this is the length of the movable
    area in pixels. Will implement a way of setting length when
    instantiating component.

--- src/componenents/RangeSlider/RangeSlider.tsx
+++ src/componenents/RangeSlider/RangeSlider.tsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect, useState } from "react";
 import { MouseInput } from "../../utils/handleMouseInput";
 import { addGenericEventListener } from "../../utils/waveformPositioning";
+import { convertPixelsToRem, convertRemToPixels } from "../../utils/utils";
 
 import "./RangeSlider.scss";
 export default function RangeSlider() {
@@ -11,10 +12,14 @@ export default function RangeSlider() {
   let lastValue: number = 0;
 
   function handleMouseMove(): void {
-    console.log("moving knub");
+    const draggableLength = 9; //rem
+    const lengthInPx = convertRemToPixels(draggableLength);
     if (mouse.mouse.isDown) {
       setValue((s) => {
-        return lastValue + mouse.mouse.x.distanceTravelled;
+        let currentVal = lastValue + mouse.mouse.x.distanceTravelled;
+        if (currentVal > lengthInPx) currentVal = lengthInPx;
+        if (currentVal < 0) currentVal = 0;
+        return currentVal;
       });
     }
   }
@@ -28,28 +33,34 @@ export default function RangeSlider() {
     }
   }
 
+  function handleValChange(ref: HTMLDivElement, length: number) {
+    let valInRem = convertPixelsToRem(value);
+    if (valInRem < 0) valInRem = 0;
+    if (valInRem > length) valInRem = length;
+    valInRem -= length;
+    ref.style.transform = `translateX(${valInRem}rem)`;
+  }
+
   useEffect(function () {
-    const handle = handleRef.current;
-    const knub = knubRef.current;
-    if (knub) {
-      console.log("knub exists");
-      if (handle) handle.style.transform = `translateX(${100 - value}px)`;
-      addGenericEventListener(knub, "mousedown", mouse.handleDown);
-      addGenericEventListener(window, "mousemove", (e: MouseEvent) =>
-        mouse.handleMove(e, knub, handleMouseMove)
-      );
-      addGenericEventListener(window, "mouseup", (e: MouseEvent) =>
-        mouse.handleUp(e, knub, handleMouseUp)
-      );
-    }
+    const handle = handleRef.current!;
+    const knub = knubRef.current!;
+    addGenericEventListener(knub, "mousedown", mouse.handleDown);
+    addGenericEventListener(window, "mousemove", (e: MouseEvent) =>
+      mouse.handleMove(e, knub, handleMouseMove)
+    );
+    addGenericEventListener(window, "mouseup", (e: MouseEvent) =>
+      mouse.handleUp(e, knub, handleMouseUp)
+    );
   }, []);
 
   useEffect(
     function () {
-      const handle = handleRef.current;
-      if (handle) {
-        handle.style.transform = `translateX(${value}px)`;
-      }
+      const handle = handleRef.current!;
+      const knub = knubRef.current!;
+      const draggableLength = convertPixelsToRem(
+        handle.offsetWidth - knub.offsetWidth
+      );
+      handleValChange(handle, draggableLength);
     },
     [value]
   );
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,12 @@ export function convertRemToPixels(rem: number): number {
   return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
 
+export function convertPixelsToRem(pixels: number): number {
+  return (
+    pixels / parseFloat(getComputedStyle(document.documentElement).fontSize)
+  );
+}
+
 export function getVariableStyle(varName: string): string {
   const style: CSSStyleDeclaration = getComputedStyle(document.body);
   return style.getPropertyValue(varName);
