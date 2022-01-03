57bfc42ce3b5a644ebed90e99bff80f96a03d001
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2022-01-03 20:32:05 +0000

    Refactor Waveform component code to manage gain changes

--- src/componenents/Waveform/Waveform.tsx
+++ src/componenents/Waveform/Waveform.tsx
@@ -1,18 +1,16 @@
 import { useEffect, useState, useRef } from "react";
 import "./Waveform.scss";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import {
-  useCreateRefs,
-  generateWaveform,
-  pixelsPerBar,
-} from "../../utils/canvas";
+import { generateWaveform, pixelsPerBar } from "../../utils/canvas";
 import { loadAudioFile } from "../../utils/loadAudioFile";
 import WaveformData from "waveform-data";
 import { handleUserInput } from "../../utils/waveformPositioning";
 import ZoomedWaveforms from "./ZoomedWaveforms/ZoomedWaveforms";
 
 export default function Waveform({ id }: { id: string }) {
-  const [canvasRef, parentRef] = useCreateRefs();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const parentRef = useRef<HTMLDivElement>(null);
+  const componentRef = useRef<HTMLDivElement>(null);
   const [waveform, setWaveform] = useState<WaveformData | null>(null);
   const { position, gain, startOffset } = useAppSelector(
     (state) => state.waveformStates[id]
@@ -24,10 +22,15 @@ export default function Waveform({ id }: { id: string }) {
     (state) => state.waveformStates[id].mouseDown
   );
   const dispatch = useAppDispatch();
+  const [waveformPosition, setWaveformPosition] = useState({
+    currentPosition: 0,
+    lastPosition: 0,
+    mouseDown: false,
+  });
 
   useEffect(function () {
     loadAudioFile(setWaveform);
-    handleUserInput(canvasRef, parentRef, id, zoomLevelRef, dispatch);
+    handleUserInput(canvasRef, componentRef, id, zoomLevelRef, dispatch);
   }, []);
 
   // draw waveform once mouse click is realeased. Prevents too many resamples of the WaveformData object and canvas redraws.
@@ -35,30 +38,51 @@ export default function Waveform({ id }: { id: string }) {
     function () {
       if (waveform) {
         const canvas = canvasRef.current!;
-        const parent = parentRef.current!;
-        if (!zoomMouseDown && !gainMouseDown) {
+        const parent = componentRef.current!;
+        if (!zoomMouseDown) {
           parent.style.width = "20000px";
           canvas.style.opacity = "1";
           const zoomedWaveform = waveform.resample({ scale: zoomLevel });
-          generateWaveform(canvasRef, parentRef, zoomedWaveform, 0, 1, 20000);
+          generateWaveform(
+            canvasRef,
+            componentRef,
+            zoomedWaveform,
+            0,
+            1,
+            20000
+          );
         }
         if (zoomMouseDown) {
           canvas.style.opacity = "0";
         }
-        if (gainMouseDown) {
-          canvas.style.transform = `scaleY(${gain})`;
-        }
       }
     },
-    [waveform, zoomMouseDown, zoomLevel, gain, gainMouseDown]
+    [waveform, zoomMouseDown, zoomLevel]
+  );
+
+  useEffect(
+    function () {
+      const canvas = canvasRef.current!;
+      canvas.style.transform = `scaleY(${gain})`;
+    },
+    [gain]
   );
 
   useEffect(
     function () {
       const pixels_per_bar = pixelsPerBar(174, zoomLevel);
-      parentRef.current!.style.left = position * pixels_per_bar + "px";
+      componentRef.current!.style.left = position * pixels_per_bar + "px";
     },
     [position, zoomLevel]
+  );
+
+  useEffect(
+    function () {
+      const pixels_per_bar = pixelsPerBar(174, zoomLevel);
+      const parent = parentRef.current!;
+      parent.style.left = startOffset * pixels_per_bar + "px";
+    },
+    [zoomLevel, startOffset]
   );
 
   useEffect(
@@ -69,13 +93,15 @@ export default function Waveform({ id }: { id: string }) {
   );
 
   return (
-    <div className="Waveform" ref={parentRef}>
-      <canvas className="Waveform__canvas" ref={canvasRef}></canvas>
-      <ZoomedWaveforms
-        parentRef={parentRef}
-        waveform={waveform ? waveform : null}
-        id="1"
-      />
+    <div className="Waveform" ref={componentRef}>
+      <div className="Waveform__content-container" ref={parentRef}>
+        <canvas className="Waveform__canvas" ref={canvasRef}></canvas>
+        <ZoomedWaveforms
+          parentRef={componentRef}
+          waveform={waveform ? waveform : null}
+          id="1"
+        />
+      </div>
     </div>
   );
 }
