ed9c032b88bb5b8c2052e2957f3b9bf3fb89727b
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2022-01-05 18:54:00 +0000

    Add redux slice to hold sequencer length in bars

---
+++ src/redux/slices/sequencerLengthSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface TotalBars {
+  length: number;
+}
+
+interface SetState {
+  value: number;
+}
+
+const initialState: TotalBars = { length: 0 };
+
+export const sequencerLengthSlice = createSlice({
+  name: "Sequencer length",
+  initialState,
+  reducers: {
+    changeLength: (state, action: PayloadAction<SetState>) => {
+      state.length = action.payload.value;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { changeLength } = sequencerLengthSlice.actions;
+
+export default sequencerLengthSlice.reducer;
