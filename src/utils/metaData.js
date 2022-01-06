a7c1a1793e11cdb6edfb0fb034a355fb82a9c9f2
Ptolemy Powlesland <ptolemypowlesland@gmail.com>
2022-01-06 12:10:13 +0000

    Add get metadata function
    Reads an audio file and returns an object containing song metadata

---
+++ src/utils/metaData.js
@@ -0,0 +1,13 @@
+import audioFile from "../audio-files/audio1.mp3";
+import mm from "musicmetadata";
+
+export async function getMetaData() {
+  const response = await fetch(audioFile);
+  const blob = await response.blob();
+  const file = new File([blob], "audio1.mp3", blob);
+  console.log(file);
+  var parser = mm(file, function (err, metadata) {
+    if (err) throw err;
+    console.log(metadata);
+  });
+}
